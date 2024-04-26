898aae5a5b6e715266c0b002e45d86bc2de0ddd4
Nima Notghi <nima.notghi@gmail.com>
2024-04-26 04:57:19 -0700

    MainMenu and GameMenu started

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -1,12 +1,35 @@
 package view;
 
+import controller.MainMenuController;
+import enums.MainMenuCommands;
+import enums.Menu;
 import model.App;
+import model.User;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+
 
 public class MainMenu extends AppMenu {
     @Override
     public void check(Scanner scanner) {
+        String input;
+        input = scanner.nextLine();
+        Matcher matcher;
 
+        MainMenuController controller = new MainMenuController();
+
+        if((matcher = MainMenuCommands.Logout.getMatcher(input)) != null){
+            User.setCurrentUser(null);
+            App.setCurrentMenu(Menu.LoginMenu);
+        } else if ((matcher = MainMenuCommands.GoToShopMenu.getMatcher(input)) != null) {
+            App.setCurrentMenu(Menu.ShopMenu);
+        } else if ((matcher = MainMenuCommands.GoToProfileMenu.getMatcher(input)) != null) {
+            App.setCurrentMenu(Menu.ProfileMenu);
+        }else if((matcher = MainMenuCommands.ShowCurrentMenu.getMatcher(input)) != null){
+            System.out.println("main menu");
+        }else {
+            System.out.println("invalid command");
+        }
     }
 }
