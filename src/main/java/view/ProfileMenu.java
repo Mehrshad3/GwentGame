019593d51749ca9f5d27f2cf3055eb0eb095bae9
M.E.Ghaseri <Mohammadermiaghaseri1384@gmail.com>
2024-04-26 15:28:31 +0330

    First ermia commit: making some changes in Profile menu(Regexes)

--- src/main/java/view/ProfileMenu.java
+++ src/main/java/view/ProfileMenu.java
@@ -1,10 +1,33 @@
 package view;
 
+import enums.ProfileMenuCommands;
+
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class ProfileMenu extends AppMenu{
     @Override
     public void check(Scanner scanner) {
+    String input;
+    input=scanner.nextLine();
+        Matcher matcher;
+
+
+    if(ProfileMenuCommands.Username.getMatcher(input) != null){
+
+    }
+    else if(ProfileMenuCommands.Nickname.getMatcher(input) != null) {
+
+    }
+    else if(ProfileMenuCommands.Email.getMatcher(input) != null){
+
+    }
+    else if(ProfileMenuCommands.Password.getMatcher(input) != null){
+
+    }
+    else{
+        System.out.println("Invalid Command!");
+    }
 
     }
 }
