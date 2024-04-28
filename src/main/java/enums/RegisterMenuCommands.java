7a9c979b209f9f091cac7bf2555357ae57931a8d
مهرشاد <meh10valiz@gmail.com>
2024-04-28 10:08:46 +0330

    RegisterMenu first command implemented. (#1)
    * Code of enum copied from Nima's 'MainMenuCommands'.
    
    * minor changes
    
    * minor changes
    
    * Some regexes added.
    
    * Regexes for valid email address and strong password added.
    
    * Documentation of phase 1 added.
    
    * Documentation of phase 1 added.
    
    * Register menu basic commands relying on User that is not implemented yet, implemented!
    
    * bug fixes in regexes of RegisterMenuCommands.
    
    * bug fixes in regexes of RegisterMenuCommands.
    
    * Bug fix in register menu regexes.

---
+++ src/main/java/enums/RegisterMenuCommands.java
@@ -0,0 +1,26 @@
+package enums;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum RegisterMenuCommands {
+    REGISTER_USER("register\\s+-u\\s+(?<username>\\S+)\\s+-p\\s+(?<password>\\S+)\\s+(?<passwordConfirm>\\S+)" +
+            "\\s+-n\\s+(?<nickname>\\S+)\\s+-e\\s+(?<email>\\S+)"),
+    REGISTER_USER_WITH_RANDOM_PASSWORD("register\\s+-u\\s+(?<username>\\S+)\\s+-p\\s+(?<password>\\S+)\\s+(?<passwordConfirm>\\S+)" +
+            "\\s+-n\\s+(?<nickname>\\S+)\\s+-e\\s+(?<email>\\S+)"),
+    PICK_QUESTION("pick\\s+question\\s+-q(?<questionNumber>(?=[1-9])[0-9]+)\\s+-a\\s+(?<answer>.+)");
+    private final Pattern pattern;
+
+    RegisterMenuCommands(String regex) {
+        this.pattern = Pattern.compile(regex);
+    }
+
+    public Matcher getMatcher(String input) {
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            return matcher;
+        }
+        return null;
+    }
+}
--- src/main/java/view/AppMenu.java
+++ src/main/java/view/AppMenu.java
@@ -3,6 +3,9 @@ package view;
 import java.util.Scanner;
 
 public abstract class AppMenu {
+    /**
+     * This method reads a single line from user input and parses it.
+     */
     public abstract void check(Scanner scanner);
 }
 
