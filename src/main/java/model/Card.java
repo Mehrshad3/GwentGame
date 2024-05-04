af37bb420441ed252dc65ad35ea4b502fd45e01e
مهرشاد <mehrshad.valizadeh03@sharif.edu>
2024-05-04 23:42:18 +0330

    amend

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -2,5 +2,9 @@ package model;
 
 abstract public class Card {
     private String name;
+    private boolean transformed = false;
 
+    public void transform() {
+        transformed = true;
+    }
 }
