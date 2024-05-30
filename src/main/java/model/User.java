4b1318f071c2877cfcd5a779bf5e738be2b0540f
M.E.Ghaseri <Mohammadermiaghaseri1384@gmail.com>
2024-05-30 13:45:38 +0330

    Profilemenucontroller completed!

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -46,6 +46,22 @@ public class User extends Player implements Serializable {
         return User.getRelativePathToFile(this.name);
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     private void saveUser() {
         // TODO
     }
